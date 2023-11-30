37d62263a6494e5fcd4cd5755472096fe8e553cb
Mozelli <joaomozelli@hotmail.com>
2023-11-30 16:01:01 -0300

    import google fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,9 +6,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt_BR">
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Nunito:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Nunito:ital,wght@0,400;1,700&display=swap" rel="stylesheet" />
+      </head>
+      
       <body>{children}</body>
     </html>
   )
