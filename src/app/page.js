fc539c61d453fa7b49947639db7b0c332d695368
Mozelli <joaomozelli@hotmail.com>
2023-12-02 14:05:53 -0300

    include Rating section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Bonus from "./components/bonus/page";
 import Hero from "./components/hero/page";
 import Info from "./components/info/page";
+import Rating from "./components/rating/page";
 import './globals.css';
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Info />
       <Bonus />
+      <Rating />
     </main>
   )
 }
